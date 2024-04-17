3214e4bec23a8270e98b84a66f684e7f38699963
Emirknt7 <emirkanat49@gmail.com>
2024-04-17 15:31:51 +0300

    kelime sayan
    c# ile yazılan  cümledeki kelime sayısını bulan kod

---
+++ cümlede kelime sayısını bulan kod/test20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {                      //Girilen Cümledeki Kelime Sayısını Bulma
+
+
+            Console.WriteLine("bir cümle girin");
+            string cümkelime = Console.ReadLine();
+            string liste = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+                           + "abcçdefgğhiıjklmnoöprsştuüvyz"+".?";
+
+            int boşluk= 1;
+
+            for (int i=0;i<cümkelime.Length;i++) {
+                if (liste.Contains(cümkelime[i]))
+                {
+                }
+                else
+                {
+                    boşluk = boşluk + 1;
+
+
+                }
+            } Console.WriteLine(boşluk);
+            Console.ReadKey();
+        }
+
+    }
+}
